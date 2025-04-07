2d4059a2afdb5946b90aa331f60645d10f414abf
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-04-06 21:03:17 -0300

    Melhorada a Lente de Gelo

--- src/main/java/net/mcreator/artinjustice/procedures/IceLenseOnTickUpdateProcedure.java
+++ src/main/java/net/mcreator/artinjustice/procedures/IceLenseOnTickUpdateProcedure.java
@@ -3,7 +3,10 @@ package net.mcreator.artinjustice.procedures;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
@@ -21,6 +24,7 @@ public class IceLenseOnTickUpdateProcedure {
 		double lx = 0;
 		double ly = 0;
 		double lz = 0;
+		boolean canBeam = false;
 		if (world.dayTime() % 24000 > 23000) {
 			angulation = 24000 - world.dayTime() % 24000;
 		} else if (world.dayTime() % 24000 < 13700) {
@@ -28,32 +32,53 @@ public class IceLenseOnTickUpdateProcedure {
 		}
 		if (angulation > 0) {
 			deg = (180 * angulation) / 14700;
+			canBeam = true;
+			Scaling = Scaling + 0.6;
 			for (int index0 = 0; index0 < 25; index0++) {
-				lx = x + -1 + Math.cos(Math.toRadians(deg)) * Scaling;
-				ly = y + -1 + Math.sin(Math.toRadians(deg)) * Scaling;
+				lx = x + 0.5 + Math.cos(Math.toRadians(deg)) * Scaling;
+				ly = y + 0.5 + Math.sin(Math.toRadians(deg)) * Scaling;
 				if (!world.getBlockState(BlockPos.containing(lx, ly, z)).canOcclude()) {
-					if (world instanceof ServerLevel _level)
-						_level.sendParticles(ParticleTypes.FLASH, x, y, z, 5, 1, 1, 1, 1);
-					{
-						final Vec3 _center = new Vec3(x, y, z);
-						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
-						for (Entity entityiterator : _entfound) {
-							if ((entityiterator.getCapability(Art5019injusticeModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new Art5019injusticeModVariables.PlayerVariables())).powerid == 23) {
-								{
-									double _setval = (entityiterator.getCapability(Art5019injusticeModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new Art5019injusticeModVariables.PlayerVariables())).powermeter + 40;
-									entityiterator.getCapability(Art5019injusticeModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-										capability.powermeter = _setval;
-										capability.syncPlayerVariables(entityiterator);
-									});
+					Scaling = Scaling + 0.9;
+				} else {
+					canBeam = false;
+					break;
+				}
+			}
+			Scaling = -0.6;
+			if (canBeam) {
+				for (int index1 = 0; index1 < 25; index1++) {
+					lx = x + 0.5 + Math.cos(Math.toRadians(deg)) * Scaling;
+					ly = y + 0.5 + Math.sin(Math.toRadians(deg)) * Scaling;
+					if (!world.getBlockState(BlockPos.containing(lx, ly, z)).canOcclude()) {
+						Scaling = Scaling - 0.6;
+					} else {
+						if (world instanceof ServerLevel _level)
+							_level.sendParticles(ParticleTypes.FLASH, lx, ly, z, 5, 1, 1, 1, 1);
+						{
+							final Vec3 _center = new Vec3(lx, ly, z);
+							List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+							for (Entity entityiterator : _entfound) {
+								if ((entityiterator.getCapability(Art5019injusticeModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new Art5019injusticeModVariables.PlayerVariables())).powerid == 23) {
+									{
+										double _setval = (entityiterator.getCapability(Art5019injusticeModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new Art5019injusticeModVariables.PlayerVariables())).powermeter + 20;
+										entityiterator.getCapability(Art5019injusticeModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+											capability.powermeter = _setval;
+											capability.syncPlayerVariables(entityiterator);
+										});
+									}
+									if (!world.isClientSide()) {
+										if (Math.random() < 0.1) {
+											if (entityiterator instanceof LivingEntity _entity && !_entity.level().isClientSide())
+												_entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 1, false, false));
+										}
+									}
 								}
 							}
 						}
+						break;
 					}
-					Scaling = Scaling - 0.6;
-				} else {
-					break;
+					GenerateDustParticlesProcedure.execute(world, lx, ly, z, "15", "0.1", "0.1", "0.1", "1.000 0.800 0.200 1", "0.1");
 				}
-				GenerateDustParticlesProcedure.execute(world, lx, ly, z, "15", "0.1", "0.1", "0.1", "1.000 0.800 0.200 1", "0.1");
 			}
 		}
 	}
